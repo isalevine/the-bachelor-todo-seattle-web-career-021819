ad719112b737cc6a5c7aeaec1d3acb3274a568c0
Isa Levine <isaacdlevine@gmail.com>
2019-01-24 03:28:13 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,32 @@
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestant_hash = data
+  first_name_winner = ""
+  contestant_hash.each do |season_key, contestant_array|
+    if season == season_key
+      contestant_array.each do |person_hash|
+        if person_hash["status"] == "Winner"
+          full_name_winner = person_hash["name"]
+          first_name_winner = full_name_winner.split(" ")[0]
+        end
+      end
+    end
+  end
+  first_name_winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_hash = {}
+  name_output = ""
+  contestant_hash.each_value do |contestant_array|
+    contestant_array.each do |person_hash|
+      if person_hash["occupation"] == occupation
+        name_output = person_hash["name"]
+      end
+    end
+  end
+  name_output
 end
 
 def count_contestants_by_hometown(data, hometown)
